e77e34582a47ae46bcde16b9f5b635c3ac4dd505
Sharunee Rajendren <sharuneeraj2000@gmail.com>
2025-03-31 20:21:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="text-blue-700"> Govisala</p>
+    </div>
+  );
+}
+
+export default App;
